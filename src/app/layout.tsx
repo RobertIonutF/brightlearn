eb93445ae3fa1126f8270063d8d7189fdcf3dc2b
RobertIonutF <robertionutfundulea@gmail.com>
2024-08-29 16:38:08 +0300

    chore: Added analytics support for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -65,6 +66,7 @@ export default function RootLayout({
               {children}
             </main>
             <Toaster />
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
